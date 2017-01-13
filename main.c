5eb374a53570c48b9ab63be0e2957202ba7c2a2a
Ian91 <none@example.com>
2017-01-12 22:27:15 -0800

    moved listener socket initialization into its own function

--- main.c
+++ main.c
@@ -7,6 +7,55 @@
 
 #define IPV4_LENGTH 16		// buffer overflow risk when using inet_ntop, etc? look into this later
 
+int initListener( char *_local_ipv4_string, uint16_t _local_port )
+{
+	struct in_addr local_ipv4;
+	int rv = inet_pton(AF_INET, _local_ipv4_string, &local_ipv4);
+	if (rv != 1)
+	{
+		printf("failed to convert local ipv4 to network format\n");
+		return -1;
+	}
+	printf("initializing listener on ip %s, port %u\n", _local_ipv4_string, _local_port);
+
+	struct sockaddr_in local_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(_local_port),
+		.sin_addr = local_ipv4
+	};
+	socklen_t local_addr_size = (socklen_t) sizeof(local_addr);
+
+	int _listening_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (_listening_sock == -1)
+	{
+		printf("failed to create listener socket\n");
+		return -1;
+	}
+	int enable = 1;
+	if (setsockopt(_listening_sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+	{    
+		printf("setsockopt(SO_REUSEADDR) failed on listener socket\n");
+		return -1;
+	}
+
+	rv = bind( _listening_sock, (const struct sockaddr *) &(local_addr), local_addr_size );
+	if (rv == -1)
+	{
+		printf("failed to bind listener socket\n");
+		return -1;
+	}
+
+	rv = listen( _listening_sock, 5 );
+	if (rv == -1)
+	{
+		printf("failed to listen on bound socket\n");
+		return -1;
+	}
+
+	return _listening_sock;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -15,64 +64,17 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("usage: ./hp <local ip> <local port>\n");
-		exit(1);
+		return 1;
 	}
-
-	struct in_addr local_ipv4;
-	rv = inet_pton(AF_INET, argv[1], &local_ipv4);
-	if (rv != 1)
-	{
-		printf("failed to convert local ipv4 to network format\n");
-		exit(1);
-	}
-
-	char local_ipv4_string[IPV4_LENGTH];
-	inet_ntop( AF_INET, &(local_ipv4), local_ipv4_string, IPV4_LENGTH );
-	if (local_ipv4_string == NULL)
-	{
-		printf("failed to convert local ipv4 to presentation format\n");
-		exit(1);
-	} 
-	printf("listening on ip %s\n", local_ipv4_string);
-
+	char *local_ipv4_string = argv[1];
 	uint16_t local_port = (uint16_t) atoi(argv[2]);
-	printf("listening on port %u\n", local_port);
 
-	struct sockaddr_in local_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(local_port),
-		.sin_addr = local_ipv4
-	};
-	socklen_t local_addr_size = (socklen_t) sizeof(local_addr);
-
-	int listening_sock = socket(AF_INET, SOCK_STREAM, 0);
+	int listening_sock = initListener( local_ipv4_string, local_port);
 	if (listening_sock == -1)
 	{
-		printf("failed to create socket\n");
-		exit(1);
+		printf("error in initializing listener\n");
+		return 1;
 	}
-	int enable = 1;
-	if (setsockopt(listening_sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-	{    
-		printf("setsockopt(SO_REUSEADDR) failed\n");
-		exit(1);
-	}
-
-	rv = bind( listening_sock, (const struct sockaddr *) &(local_addr), local_addr_size );
-	if (rv == -1)
-	{
-		printf("failed to bind socket\n");
-		exit(1);
-	}
-	printf("bound listening socket\n");
-
-	rv = listen( listening_sock, 5 );
-	if (rv == -1)
-	{
-		printf("failed to listen on bound socket\n");
-		exit(1);
-	}
-	printf("listened successfully\n");
 
 	struct sockaddr_in remote_addr;
 	socklen_t remote_addr_size = (socklen_t) sizeof(remote_addr);
@@ -81,20 +83,20 @@ int main(int argc, char *argv[])
 	if (conn_sock == -1)
 	{
 		printf("failed to accept connections\n");
-		exit(1);
+		return 1;
 	}
 	rv = getpeername(conn_sock, (struct sockaddr *) &(remote_addr), &(remote_addr_size) );
 	if (rv == -1)
 	{
 		printf("failed to get connected socket's info\n");
-		exit(1);
+		return 1;
 	}
 	char remote_ipv4_string[IPV4_LENGTH];
 	inet_ntop( AF_INET, &(remote_addr.sin_addr), remote_ipv4_string, IPV4_LENGTH );
 	if (remote_ipv4_string == NULL)
 	{
 		printf("failed to convert remote ipv4 to presentation format\n");
-		exit(1);
+		return 1;
 	}
 	printf("got connection from %s\n", remote_ipv4_string);
 
